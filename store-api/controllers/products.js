66c870ddc57f437df4d50cfb120379196851c35c
divyaj2701 <divyaj0127@gmail.com>
2025-08-14 23:51:45 +0530

    filter methods

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,11 +1,29 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing Async errors')         // no need to use next
-    res.status(200).json({msg:'Product Testing route'})
+    // const products = await Product.find({featured:true})
+    const search = 'ab'
+    const products = await Product.find({
+        // name:'entertainment center'                          // exact matching
+        name : {$regex : search, $options: 'i'},                // pattern matching, i->case insensitive
+    })
+    res.status(200).json({msg:products, nbHits: products.length})
+    // throw new Error('testing Async errors')         // no need to use next
 }
 
 const getAllProducts= async (req, res) => {
-    res.status(200).json({msg:'Product route'})
-}
+
+    const {featured, company, name} = req.query
+    const queryObject = {}                          // to remove unnecessary query
+
+    if(featured) queryObject.featured = featured === 'true'? true:false;
+    if(company) queryObject.company = company;
+    if(name) queryObject.name = {$regex : name, $options: 'i'};
+    console.log(queryObject);
+    
+    const products = await Product.find(queryObject)
+    res.status(200).json({msg:products, nbHits: products.length})
+} 
 
 module.exports = {
     getAllProductsStatic,
