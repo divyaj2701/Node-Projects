1996dd7352cd8e56e465865c1786a6c1b5d0505d
divyaj2701 <divyaj0127@gmail.com>
2025-08-15 18:01:20 +0530

    ui, error, notFound setup

--- JWT-Basics/app.js
+++ JWT-Basics/app.js
@@ -1,1 +1,29 @@
-console.log("setup done")
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+// middleware
+app.use(express.static('./public'))          // index.html, .css etc. files
+app.use(express.json())                     // there is post route => to access req.body
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error); 
+    }
+}
+
+start()
