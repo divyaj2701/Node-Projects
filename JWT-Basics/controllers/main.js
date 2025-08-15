fc59abb7d04c4b7d41e566d63377b95b37767dd6
divyaj2701 <divyaj0127@gmail.com>
2025-08-15 19:40:44 +0530

    some info

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -1,5 +1,25 @@
+// check for username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+
+// setup authentication so only the request with JWT can access the dashboard
+
+
+const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
+    const {username, password} = req.body
+    // console.log(username, password);                 
+
+    // mongo - mongo's required validation 
+    // Joi - set up entire additonal layer of validation, using another package i.e. Joi
+    // check in the controller
+
+
+    if(!username || !password){
+        throw new CustomAPIError('Please Provide email and password', 400)     // 400 -> Bad request
+    }
+
     res.send('Fake Login/Register/SignUp Route')
 }
 
