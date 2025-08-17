00a56d2ad98fc9a2e83c0df49ceea9c10f488560
divyaj2701 <divyaj0127@gmail.com>
2025-08-17 22:28:43 +0530

    controllers and routes setup

--- Jobs-API/app.js
+++ Jobs-API/app.js
@@ -5,6 +5,11 @@ const express = require('express')
 const app = express();
 
 const connectDB = require('./db/connect')
+
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -12,9 +17,8 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(express.json())
 
 //routes
-app.get('/', (req,res)=>{
-    res.send('send jobs')
-})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs',jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
